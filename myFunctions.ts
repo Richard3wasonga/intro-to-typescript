f679d1e495f53669e89a30a0549065449bc6baaa
richard <richard.wasonga@student.moringaschool.com>
2025-04-27 14:23:30 +0300

    lesson two function

--- myFunctions.ts
+++ myFunctions.ts
@@ -1,6 +1,7 @@
-function addTwo (num: number){
+function addTwo (num: number): number{
 
     return num + 2
+    //return 'hello'
 }
 
 function getUpper(val: string){
@@ -14,5 +15,35 @@ addTwo(5)
 getUpper("row")
 signUpUser("Row","row@dev.com",true)
 loginUser("row", "row@dev.com")
+
+//functions returning more than one data type
+function getValue(myVal: number){
+    if(myVal > 5){
+        return true
+    }
+    return "200 ok"
+}
+
+const getHello = (s: string): string => {
+    return ""
+}
+
+const heros = ["thoe", "spiderman", "ironman"]
+//const heros = [1,2,3]
+
+heros.map((hero): string => {
+    return `hero is ${hero}`
+})
+
+function consoleError (ereemsg: string): void{
+    console.log(ereemsg)
+    
+}
+
+function handleError (ereemsg: string): never{
+    throw new Error(ereemsg)
+    
+}
+
 
 export {}
